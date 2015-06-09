023b4ccdbcab659e3dc013224da03ea36217c8e2
Jianwei Han <hanjianwei@gmail.com>
2015-06-09 17:41:44 +0800

    Add default options

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # zsh-sensible
-Default zsh options
+Default zsh options.
+
+## Usage
+
+Setup using [Antigen](http://antigen.sharats.me):
+
+~~~ sh
+antigen bundle hanjianwei/zsh-sensible
+antigen apply
+~~~
---
+++ sensible.zsh
@@ -0,0 +1,29 @@
+#
+# Utilities
+#
+
+# Colorful ls
+if [[ "$OSTYPE" == darwin* ]]; then
+    alias ls='ls -G'
+else
+    alias ls='ls --color=auto'
+fi
+
+#
+# Completion
+#
+autoload -Uz compinit && compinit -i
+
+setopt COMPLETE_IN_WORD    # Complete from both ends of a word.
+setopt PATH_DIRS           # Perform path search even on command names with slashes.
+unsetopt CASE_GLOB         # Case-insensitive matching
+
+# Colors
+export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+
+# Partial-word, and then substring completion.
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+# Allow arrow selection
+zstyle ':completion:*:*:*:*:*' menu select
